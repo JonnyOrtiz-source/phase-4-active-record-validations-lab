b664f78c67898fb0131db00d934bddf564a8b740
Jonny Ortiz <j0nny.0rt1z@yahoo.com>
2022-07-17 15:58:17 -0400

    Completed assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+
+    validate :title_is_clickbait?
+
+    CLICKBAIT = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i
+    ]
+
+    def title_is_clickbait?
+        if CLICKBAIT.none? {|titleEl| titleEl.match title} 
+            errors.add(:title, "not clickbait-y enough")
+        end
+    end
+
 end
